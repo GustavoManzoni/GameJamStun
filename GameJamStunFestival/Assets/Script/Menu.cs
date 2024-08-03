d006ab6364e29e08ae47633a5e92588a52205dc3
elp28 <elpbr2807@gmail.com>
2024-08-03 10:59:45 -0300

    Menu Teste

---
+++ GameJamStunFestival/Assets/Script/Menu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public string nomeDoLeveldeJogo;
+    public GameObject painelMenuInicial;
+    public GameObject painelOpcoes;
+    public GameObject sair;
+    public GameObject play;
+    public GameObject abrirMenu;
+
+    private float volume = 1.0f; // Volume inicial
+
+    public void Jogar()
+    {
+        SceneManager.LoadScene(".");
+    }
+    public void AbrirOpcoes()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void AbrirJogo()
+    {
+        play.SetActive(true);
+    }
+
+    public void FecharOpcoes()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void SairJogo()
+    {
+        Application.Quit();
+        Debug.Log("Saiu do jogo");
+    }
+
+    public void AbrirMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
